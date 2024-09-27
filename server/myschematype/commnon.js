647ecadf9ecb48b31403e631daeaab2d385f1a23
Sovitsaini143 <sovitsaini37@gmail.com>
2024-09-27 11:02:06 +0530

    change

---
+++ server/myschematype/commnon.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mynew = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    age:{},
+    
+
+});
+
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const apps = express();
+require('dotenv').config();
+const myroute = require('./routes/myroute');
+const cors = require('cors');
+const myport = process.env.PORT || 8700 
+require('./database/connection');   
+
+
+
+apps.use(express.json());
+apps.use(myroute);
+apps.use(cors);
+
+
+
+apps.listen(myport,()=>{
+    console.log(`server is running at port no: ${myport}`);
+
+});
